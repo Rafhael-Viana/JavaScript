875cb593fa87f08daba90286628f6035862543f7
Rafhael-Viana <vnarafha@gmail.com>
2023-06-13 10:04:16 -0300

    Atualização pasta Aula13

---
+++ Aula13/ambiente.js
@@ -0,0 +1,35 @@
+// A variável foi definida dentro da estrutura
+console.log('Vai começar...')
+for( var c = 1; c <=5; c++){
+    console.log(c)
+}
+console.log("FIM!!!")
+
+
+
+
+
+
+
+
+
+// do{
+//     console.log(`Passo ${c}`)
+//     c++
+// }while(c <= 10)
+
+
+
+
+
+
+// while(c <= 500){
+//     console.log(`Número ${c}`)
+//     c++
+// }
+
+
+
+
+
+// console.log("Td bem?")
