a0c8bb03bb705e864c613544b414c7a231c24df9
Rafhael-Viana <vnarafha@gmail.com>
2023-06-13 22:24:25 -0300

    Resolução ex021

---
+++ Aula14ex/ex021/script.js
@@ -0,0 +1,23 @@
+let b = document.querySelector('input.button')
+b.addEventListener('click', gerar)
+
+function gerar() {
+    let n = document.getElementById('num')
+    let tb = document.getElementById('sel')
+    let res = document.querySelector('div#res')
+    if(n.value.length == 0){
+        window.alert('Por favor, digite um número')
+    }else{
+        let p = 1
+        n = Number(n.value)
+        tb.innerHTML = ''
+        // res.innerHTML = 'Tabuada:'
+        for(let c = n ; c <= (n*10); c += n){
+            let item = document.createElement('option')
+            item.text = `${n} x ${p} =  ${c}`
+            item.value = `v${p}`
+            tb.appendChild(item)
+            p++
+        }
+    }
+}
