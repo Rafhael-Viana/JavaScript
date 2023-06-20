273c855bf6fabca5c9c84bec747554a1c08782aa
Rafhael-Viana <vnarafha@gmail.com>
2023-06-20 10:01:11 -0300

    Update ambiente.js

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -4,9 +4,9 @@ console.log(`Nossa template string é ${num}`)
 console.log(`A string tem ${num.length} posições.`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
-// for(let p = 0; p<num.length; p++){
-//     console.log(num[p])
-// }
+for(let p = 0; p<num.length; p++){
+    console.log(num[p])
+}
 
 // for(let p in num){
 //     console.log(`A posição ${p} tem o valor ${num[p]}`)
