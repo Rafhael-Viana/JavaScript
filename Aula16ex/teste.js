edc9003de4ebb1329a30be21c07527c273e12132
Rafhael-Viana <vnarafha@gmail.com>
2023-07-02 12:25:51 -0300

    Mudanças arquivos JSON

--- Aula16ex/teste.js
+++ Aula16ex/teste.js
@@ -1,9 +1,16 @@
-let lista = [3,4,5,6,7]
+// let lista = [3,4,5,6,7]
 
-// for(let c = 1;c<lista.length;c++){
-//     lista[0] += lista[c] 
-// }
+// // for(let c = 1;c<lista.length;c++){
+// //     lista[0] += lista[c]
+// // }
 
-// console.log(lista[0])
+// // console.log(lista[0])
 
-console.log(lista.length)
+// console.log(lista.length)
+
+const arrowTest = (test) => {
+    const lista = {
+        name: "Jose",
+        idade: 42,
+    };
+};
