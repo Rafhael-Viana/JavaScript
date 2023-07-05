73f875b6dbe7642ca77829db5326870d1aa03507
Rafhael-Viana <vnarafha@gmail.com>
2023-07-05 19:42:11 -0300

    Propriedades do Document

---
+++ Aula19 - Propriedades do Document/document.js
@@ -0,0 +1,8 @@
+//Propriedades Document
+console.log(document.body) ;
+console.log(document.head) ;
+console.log(document.links [0] ) ;
+document.links[0].textContent = 'Twitter'
+console.log(document.URL) ;
+document.title = "Aula 19"
+console.log(document.title) ;
