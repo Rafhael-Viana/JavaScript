8be2faa0a9f29024f2c133779d782072aebe5516
Rafhael-Viana <vnarafha@gmail.com>
2023-07-07 00:05:25 -0300

    Criação Aula21

---
+++ Aula21 - setTimeout e setInteval/set.js
@@ -0,0 +1,16 @@
+//setTimeout
+//executa o código com delay predefinido
+setTimeout(() => {
+    console.log('Testando o setTimeout')
+}, 100) // 100ms
+
+console.log("Isso vem primeiro mesmo vindo depois")
+
+//setInterval
+//Define um timer para executar uma função.Funciona como um loop
+
+    setInterval(() => {
+    console.log('Testando o setInterval')
+    }, 2000) // obs: Funciona infinitamente. Cuidado!!!
+
+
